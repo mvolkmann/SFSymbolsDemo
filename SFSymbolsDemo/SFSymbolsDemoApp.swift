4d2f31d51d032a7026688197de6318da3d543ee3
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-18 07:19:54 -0500

    Initial Commit

---
+++ SFSymbolsDemo/SFSymbolsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolsDemoApp.swift
+//  SFSymbolsDemo
+//
+//  Created by Mark Volkmann on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
